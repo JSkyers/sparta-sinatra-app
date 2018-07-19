90541b8b451bf7a2f77b0ff6ecaf90c7becb6902
unknown <jskyers@spartaglobal.com>
2018-07-19 23:25:58 +0100

    make interface clearer

--- public/js/global.js
+++ public/js/global.js
@@ -1,9 +1,9 @@
 var create_audio = new Audio('audio/create_a_character.wav');
 var main_menu_audio = new Audio('audio/main_menu.wav');
 
-$(".new_character_logo").click(function () {
+$(".new_character_logo").hover(function () {
   create_audio.play();
 });
-$(".home_page_logo").click(function () {
+$(".home_page_logo").hover(function () {
   main_menu_audio.play();
 });
